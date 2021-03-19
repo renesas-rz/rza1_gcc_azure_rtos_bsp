a215f9aef7a4a5f69a3199feff1d0a03fa8875c0
mkosinski01 <zkmike@gmail.com>
2021-03-19 16:54:36 -0700

    Sample Project to show how to use the OS Abstraction Layer.

--- r_sample_threadx/src/sample_threadx.c
+++ r_sample_threadx/src/sample_threadx.c
@@ -21,9 +21,9 @@ os_task_t               thread_4;
 os_task_t               thread_5;
 os_task_t               thread_6;
 os_task_t               thread_7;
-TX_QUEUE                queue_0;
-TX_SEMAPHORE            semaphore_0;
-TX_MUTEX                mutex_0;
+os_msg_queue_handle_t   queue_0;
+semaphore_t             semaphore_0;
+event_t	                mutex_0;
 event_t	    			event_flags_0;
 TX_BYTE_POOL            byte_pool_0;
 TX_BLOCK_POOL           block_pool_0;
@@ -68,24 +68,48 @@ int main(void)
 void    tx_application_define(void *first_unused_memory)
 {
 	uint32_t param;
+	/* Create the main thread.  */
 	param = 0;
 	R_OS_CreateTask ( "thread 0", thread_0_entry, 		(void*)&param, DEMO_STACK_SIZE, 1);
+
+	/* Create threads 1 and 2. These threads pass information through a ThreadX
+	   message queue.  It is also interesting to note that these threads have a time
+	slice.  */
 	param = 1;
-	//R_OS_CreateTask ( "thread 1", thread_1_entry, 		(void*)&param, DEMO_STACK_SIZE, 16);
+	R_OS_CreateTask ( "thread 1", thread_1_entry, 		(void*)&param, DEMO_STACK_SIZE, 16);
 	param = 2;
-	//R_OS_CreateTask ( "thread 2", thread_2_entry, 		(void*)&param, DEMO_STACK_SIZE, 16);
+	R_OS_CreateTask ( "thread 2", thread_2_entry, 		(void*)&param, DEMO_STACK_SIZE, 16);
+
+	/* Create threads 3 and 4.  These threads compete for a ThreadX counting semaphore.
+   	   An interesting thing here is that both threads share the same instruction area.  */
 	param = 3;
-	//R_OS_CreateTask ( "thread 3", thread_3_and_4_entry, (void*)&param, DEMO_STACK_SIZE, 8);
+	R_OS_CreateTask ( "thread 3", thread_3_and_4_entry, (void*)&param, DEMO_STACK_SIZE, 8);
 	param = 4;
-	//R_OS_CreateTask ( "thread 4", thread_3_and_4_entry, (void*)&param, DEMO_STACK_SIZE, 8);
+	R_OS_CreateTask ( "thread 4", thread_3_and_4_entry, (void*)&param, DEMO_STACK_SIZE, 8);
+
+	/* Create thread 5.  This thread simply pends on an event flag which will be set
+	   by thread_0.  */
 	param = 5;
 	R_OS_CreateTask ( "thread 5", thread_5_entry, 		(void*)&param, DEMO_STACK_SIZE, 4);
-	param = 6;
-	//R_OS_CreateTask ( "thread 6", thread_6_and_7_entry, (void*)&param, DEMO_STACK_SIZE, 8);
-	param = 7;
-	//R_OS_CreateTask ( "thread 1", thread_6_and_7_entry, (void*)&param, DEMO_STACK_SIZE, 8);
 
+	/* Create threads 6 and 7.  These threads compete for a ThreadX mutex.  */
+	param = 6;
+	R_OS_CreateTask ( "thread 6", thread_6_and_7_entry, (void*)&param, DEMO_STACK_SIZE, 8);
+	param = 7;
+	R_OS_CreateTask ( "thread 1", thread_6_and_7_entry, (void*)&param, DEMO_STACK_SIZE, 8);
+
+	/* Create the event flags group used by threads 1 and 5.  */
 	R_OS_CreateEvent ( &event_flags_0 );
+
+	/* Create the message queue shared by threads 1 and 2.  */
+	R_OS_CreateMessageQueue( DEMO_QUEUE_SIZE, &queue_0 );
+
+	/* Create the semaphore used by threads 3 and 4.  */
+	R_OS_CreateSemaphore(&semaphore_0, 1);
+
+	/* Create the mutex used by thread 6 and 7 without priority inheritance.  */
+	mutex_0 = R_OS_CreateMutex();
+
 #if 0
 CHAR    *pointer;
 
@@ -205,10 +229,10 @@ void    thread_0_entry(ULONG thread_input)
         thread_0_counter++;
 
         /* Sleep for 10 ticks.  */
-        tx_thread_sleep(10);
+        R_OS_TaskSleep(10);
 
         /* Set event flag 0 to wakeup thread 5.  */
-        if (!R_OS_SetEvent( &event_flags_0 ) )
+        if (!R_OS_SetEvent( event_flags_0 ) )
         	break;
 
     }
@@ -218,53 +242,55 @@ void    thread_0_entry(ULONG thread_input)
 void    thread_1_entry(ULONG thread_input)
 {
 
-UINT    status;
+	thread_1_messages_sent = 0;
+
+	/* This thread retrieves messages placed on the queue by thread 1.  */
+	while(1)
+	{
+
+		/* Increment the thread counter.  */
+		thread_1_counter++;
+
+		/* Sleep for 10 ticks.  */
+		R_OS_TaskSleep(10);
+
+		/* Retrieve a message from the queue.  */
+		if ( !R_OS_PutMessageQueue(queue_0, (void*)&thread_1_messages_sent )) {
+			/* Check completion status.  */
+			break;
+		}
 
 
-    /* This thread simply sends messages to a queue shared by thread 2.  */
-    while(1)
-    {
+		/* Increment the message sent.  */
+		thread_1_messages_sent++;
+	}
 
-        /* Increment the thread counter.  */
-        thread_1_counter++;
-
-        /* Send message to queue 0.  */
-        status =  tx_queue_send(&queue_0, &thread_1_messages_sent, TX_WAIT_FOREVER);
-
-        /* Check completion status.  */
-        if (status != TX_SUCCESS)
-            break;
-
-        /* Increment the message sent.  */
-        thread_1_messages_sent++;
-    }
 }
 
 
 void    thread_2_entry(ULONG thread_input)
 {
 
-ULONG   received_message;
-UINT    status;
+	ULONG   received_message;
+	UINT    status;
 
-    /* This thread retrieves messages placed on the queue by thread 1.  */
-    while(1)
-    {
+	    /* This thread retrieves messages placed on the queue by thread 1.  */
+	    while(1)
+	    {
 
-        /* Increment the thread counter.  */
-        thread_2_counter++;
+	        /* Increment the thread counter.  */
+	        thread_2_counter++;
 
-        /* Retrieve a message from the queue.  */
-        status = tx_queue_receive(&queue_0, &received_message, TX_WAIT_FOREVER);
+	        /* Retrieve a message from the queue.  */
+	        if ( !R_OS_GetMessageQueue(queue_0, &received_message, TX_WAIT_FOREVER, true)) {
+	        	/* Check completion status and make sure the message is what we
+	        		           expected.  */
+	        	break;
+	        }
 
-        /* Check completion status and make sure the message is what we 
-           expected.  */
-        if ((status != TX_SUCCESS) || (received_message != thread_2_messages_received))
-            break;
-        
-        /* Otherwise, all is okay.  Increment the received message count.  */
-        thread_2_messages_received++;
-    }
+	        /* Otherwise, all is okay.  Increment the received message count.  */
+	        thread_2_messages_received++;
+	    }
 }
 
 
@@ -286,21 +312,20 @@ UINT    status;
             thread_4_counter++;
 
         /* Get the semaphore with suspension.  */
-        status =  tx_semaphore_get(&semaphore_0, TX_WAIT_FOREVER);
-
-        /* Check status.  */
-        if (status != TX_SUCCESS)
-            break;
+        if ( !R_OS_WaitForSemaphore( semaphore_0, TX_WAIT_FOREVER)) {
+        	/* Check status.  */
+        	break;
+        }
 
         /* Sleep for 2 ticks to hold the semaphore.  */
-        tx_thread_sleep(2);
+        R_OS_TaskSleep(2);
 
         /* Release the semaphore.  */
-        status =  tx_semaphore_put(&semaphore_0);
+        if ( !R_OS_ReleaseSemaphore( semaphore_0) ) {
+        	/* Check status.  */
+        	break;
+        }
 
-        /* Check status.  */
-        if (status != TX_SUCCESS)
-            break;
     }
 }
 
@@ -320,7 +345,7 @@ ULONG   actual_flags;
         thread_5_counter++;
 
         /* Wait for event flag 0.  */
-        actual_flags = R_OS_WaitForEvent ( &event_flags_0, R_OS_ABSTRACTION_PRV_EV_WAIT_INFINITE);
+        actual_flags = R_OS_WaitForEvent ( event_flags_0, R_OS_ABSTRACTION_PRV_EV_WAIT_INFINITE);
 
 
         /* Check status.  */
@@ -348,37 +373,32 @@ UINT    status;
             thread_7_counter++;
 
         /* Get the mutex with suspension.  */
-        status =  tx_mutex_get(&mutex_0, TX_WAIT_FOREVER);
-
-        /* Check status.  */
-        if (status != TX_SUCCESS)
-            break;
+        if ( !R_OS_AcquireMutex(mutex_0)) {
+        	/* Check status.  */
+        	break;
+        }
 
         /* Get the mutex again with suspension.  This shows
            that an owning thread may retrieve the mutex it
            owns multiple times.  */
-        status =  tx_mutex_get(&mutex_0, TX_WAIT_FOREVER);
-
-        /* Check status.  */
-        if (status != TX_SUCCESS)
-            break;
+        if ( !R_OS_AcquireMutex(mutex_0)) {
+			/* Check status.  */
+			break;
+		}
 
         /* Sleep for 2 ticks to hold the mutex.  */
-        tx_thread_sleep(2);
+        R_OS_TaskSleep(2);
 
         /* Release the mutex.  */
-        status =  tx_mutex_put(&mutex_0);
+        if ( !R_OS_ReleaseMutex(mutex_0) ) {
+        	break;
+        }
 
-        /* Check status.  */
-        if (status != TX_SUCCESS)
-            break;
 
         /* Release the mutex again.  This will actually 
            release ownership since it was obtained twice.  */
-        status =  tx_mutex_put(&mutex_0);
-
-        /* Check status.  */
-        if (status != TX_SUCCESS)
-            break;
+        if ( !R_OS_ReleaseMutex(mutex_0) ) {
+        	break;
+        }
     }
 }
