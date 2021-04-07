caae8c8effcde1e25df05394dc2a4a776d000001
mkosinski01 <zkmike@gmail.com>
2021-04-06 22:33:05 -0700

    untracked object files

---
+++ guix/Debug/rz_rsk_touch.d
@@ -0,0 +1,187 @@
+rz_rsk_touch.o: ../rz_rsk_touch.c \
+ ../../rz_bsp/src/renesas/application/system/inc/r_typedefs.h \
+ ../../rz_bsp/src/renesas/application/system/inc/iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/iodefine_typedef.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/adc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/bsc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/ceu_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/cpg_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/disc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/dmac_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/dvdec_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/ether_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/flctl_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/gpio_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/ieb_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/inb_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/intc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/irda_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/jcu_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/l2c_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/lin_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/lvds_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/mlb_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/mmc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/mtu2_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/ostm_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/pfv_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/pwm_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/riic_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/romdec_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/rscan0_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/rspi_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/rtc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/scif_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/scim_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/scux_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/sdg_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/spdif_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/spibsc_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/ssif_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/usb20_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/vdc5_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/iodefines/wdt_iodefine.h \
+ ../../rz_bsp/src/renesas/application/system/inc/dev_drv.h \
+ ../../rz_bsp/src/renesas/drivers/r_i2c/inc/r_riic_api.h \
+ ../../rz_bsp/src/renesas/drivers/intc/inc/r_intc.h \
+ ../../rz_bsp/src/renesas/application/system/inc/rza_io_regrw.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/cpg_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/intc_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/bsc_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/dmac_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/mtu2_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/ostm_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/scif_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/rspi_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/riic_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/adc_iobitmask.h \
+ ../../rz_bsp/src/renesas/application/system/iobitmasks/gpio_iobitmask.h \
+ ../../rz_bsp/src/renesas/middleware/touch/inc/tp_if.h \
+ ../../threadx/common/inc/tx_api.h \
+ ../../rz_bsp/azure/ports/cortex_a9/gnu/inc/tx_port.h \
+ ../../guix/common/inc/gx_api.h \
+ ../../rz_bsp/azure/ports/cortex_a9/gnu/inc/gx_port.h
+
+../../rz_bsp/src/renesas/application/system/inc/r_typedefs.h:
+
+../../rz_bsp/src/renesas/application/system/inc/iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/iodefine_typedef.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/adc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/bsc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/ceu_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/cpg_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/disc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/dmac_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/dvdec_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/ether_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/flctl_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/gpio_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/ieb_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/inb_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/intc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/irda_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/jcu_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/l2c_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/lin_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/lvds_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/mlb_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/mmc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/mtu2_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/ostm_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/pfv_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/pwm_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/riic_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/romdec_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/rscan0_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/rspi_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/rtc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/scif_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/scim_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/scux_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/sdg_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/spdif_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/spibsc_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/ssif_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/usb20_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/vdc5_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/iodefines/wdt_iodefine.h:
+
+../../rz_bsp/src/renesas/application/system/inc/dev_drv.h:
+
+../../rz_bsp/src/renesas/drivers/r_i2c/inc/r_riic_api.h:
+
+../../rz_bsp/src/renesas/drivers/intc/inc/r_intc.h:
+
+../../rz_bsp/src/renesas/application/system/inc/rza_io_regrw.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/cpg_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/intc_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/bsc_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/dmac_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/mtu2_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/ostm_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/scif_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/rspi_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/riic_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/adc_iobitmask.h:
+
+../../rz_bsp/src/renesas/application/system/iobitmasks/gpio_iobitmask.h:
+
+../../rz_bsp/src/renesas/middleware/touch/inc/tp_if.h:
+
+../../threadx/common/inc/tx_api.h:
+
+../../rz_bsp/azure/ports/cortex_a9/gnu/inc/tx_port.h:
+
+../../guix/common/inc/gx_api.h:
+
+../../rz_bsp/azure/ports/cortex_a9/gnu/inc/gx_port.h:
