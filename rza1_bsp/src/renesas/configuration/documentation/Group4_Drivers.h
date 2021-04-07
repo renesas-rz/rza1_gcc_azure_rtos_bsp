648d6043105b1dfd7d8674150bee0eb1a36b727c
mkosinski01 <zkmike@gmail.com>
2021-04-06 20:48:20 -0700

    Removed RZA1H bsp

--- rza1_bsp/src/renesas/configuration/documentation/Group4_Drivers.h
+++
@@ -1,6 +0,0 @@
-﻿/* Character Encoding: "WHITE SQUARE" U+25A1 is □. */
-/*!
-@ingroup RENESAS_APPLICATION_SOFTWARE_PACKAGE Software Package
-@defgroup  R_SW_PKG_93_NONOS_DRIVERS Drivers (Not POSIX)
-@brief     Drivers
-*/
