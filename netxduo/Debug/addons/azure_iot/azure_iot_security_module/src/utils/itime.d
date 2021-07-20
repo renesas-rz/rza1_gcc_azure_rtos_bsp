c0532d39456cf15f5b0b7fe02f7108cea2e169df
mkosinski01 <zkmike@gmail.com>
2021-07-20 16:55:07 -0700

    Removed object files from NeTxDuo libraries. moved the header file nx_api.h to rz_bsp project to build the sample

--- netxduo/Debug/addons/azure_iot/azure_iot_security_module/src/utils/itime.d
+++
@@ -1,5 +0,0 @@
-addons/azure_iot/azure_iot_security_module/src/utils/itime.o: \
- ../addons/azure_iot/azure_iot_security_module/src/utils/itime.c \
- ../../netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/inc/asc_security_core/utils/itime.h
-
-../../netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/inc/asc_security_core/utils/itime.h:
--- netxduo/Debug/addons/azure_iot/azure_iot_security_module/src/utils/iuuid.d
+++
@@ -1,8 +0,0 @@
-addons/azure_iot/azure_iot_security_module/src/utils/iuuid.o: \
- ../addons/azure_iot/azure_iot_security_module/src/utils/iuuid.c \
- ../../netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/inc/asc_security_core/utils/irand.h \
- ../../netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/inc/asc_security_core/utils/iuuid.h
-
-../../netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/inc/asc_security_core/utils/irand.h:
-
-../../netxduo/addons/azure_iot/azure_iot_security_module/iot-security-module-core/inc/asc_security_core/utils/iuuid.h:
